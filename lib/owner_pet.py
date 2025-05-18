b89e9cc086b27f0e043d85bbf13d1a726aeac372
Daniel Kipngetich <danielkipngetich10@gmail.com>
2025-05-18 06:59:01 +0300

    adds one to many relationship in owner_pet.py

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,38 @@
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = [] 
+
+    def __init__(self, name, pet_type, owner=None):
+        if pet_type not in self.PET_TYPES:
+            raise Exception(f"{pet_type} is not a valid pet type.")
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = None  
+        if owner:
+            self.set_owner(owner) 
+        Pet.all.append(self) 
+
+    def set_owner(self, owner):
+        if not isinstance(owner, Owner):
+            raise Exception("Owner must be an instance of the Owner class.")
+        self.owner = owner
+        owner.add_pet(self)
+
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name 
+        self._pets = [] 
+
+    def pets(self):
+        return self._pets
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("Only instances of Pet can be added.")
+        pet.owner = self 
+        self._pets.append(pet) 
+
+
+    def get_sorted_pets(self):
+        return sorted(self._pets, key=lambda pet: pet.name)
